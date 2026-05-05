34fb57c3b94a1cd90aa07431d87dc64116708878
Pete Green <p.l.green@liverpool.ac.uk>
2026-05-05 18:56:26 +0100

    updating parameters

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 from RFFGaussianProcess import RFFGaussianProcess1D
 
-X = np.linspace(0, 10, 30)
-X_star = np.linspace(0, 10, 30)
+X = np.linspace(0, 10, 100)
+X_star = np.linspace(-1, 11, 100)
 y = np.sin(X) + 0.1 * np.random.randn(len(X))
 
 m = RFFGaussianProcess1D(noise_var=0.01)
@@ -11,7 +11,7 @@ m.train(X=X, y=y)
 y_star_mean, y_star_var = m.predict(X_star)
 
 fig, ax = plt.subplots()
-ax.plot(X, y, 'o')
+ax.plot(X, y, 'o', color='black')
 ax.plot(X_star, y_star_mean, color='red')
 ax.plot(X_star, y_star_mean + 3 * np.sqrt(y_star_var), color='red')
 ax.plot(X_star, y_star_mean - 3 * np.sqrt(y_star_var), color='red')
